25f6420000c8d365ac2f07931e0f5c67376d007c
Chudier <chudierp@gmail.com>
2019-10-16 13:33:53 -0700

    added login form

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+        return home()
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()        
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
