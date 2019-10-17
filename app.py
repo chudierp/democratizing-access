4a8fb11f0c872bb569e299612c1e259a6c51cf06
Chudier <chudierp@gmail.com>
2019-10-17 10:05:25 -0700

    added a signup form

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
-from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
-import os
+# import os
+from flask_wtf import FlaskForm, Form
+from flask import Flask, redirect, render_template, url_for
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, PasswordField, BooleanField 
+from wtforms.validators import InputRequired, Email, Length
+
 
 app = Flask(__name__)
+Bootstrap(app)
+app.config['SECRET_KEY'] = 'Thisissupposetobesecret'
 
-@app.route('/')
-def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return "Hello Boss!"
 
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-        return home()
+class LoginForm(FlaskForm):
+    username = StringField("username",validators=[InputRequired(), Length(min=4, max=15,)])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=6, max=80)])
+    # remember = BooleanField('remember me')
 
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return home()        
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    return render_template('register.html', form=form)        
+
+     
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    print(app.secret_key)
+    app.run(debug=True)
